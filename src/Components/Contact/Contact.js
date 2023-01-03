86a840c63ca010c083896fdf16421a2236150a74
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2023-01-03 15:15:07 +0300

    contact info

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ContactInfo from './ContactInfo';
+import './Contact.css';
+import { Container } from '@mui/material';
+
+function Contact() {
+  return (
+    <div className='Contact'>
+        Contact
+<div className='Contact-container'>
+
+<div>
+  <img  className='contact-image' src='/images/contact.jpg'/>
+</div>
+
+<Container maxWidth='xl'>
+  <ContactInfo />
+</Container>
+
+</div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/Components/Contact/ContactInfo.js
@@ -0,0 +1,38 @@
+import { Container } from '@mui/material';
+import React from 'react';
+import './Contact.css';
+
+function ContactInfo() {
+  return (
+    <div className='contact-Info'>
+        <p className='contact-head'>Let's Work Together. Get in Touch Using the Form Below.</p>
+<Container maxWidth='xl'>
+<form className='contact-form'>
+<div className='contact-div'>
+<label className='label' >Name*</label><br />
+    <input placeholder='first name' type='text' style={{marginBottom:"2px"}}/>
+    <input  placeholder='last name' type='text'/>
+</div>  
+
+<div className='contact-div'>
+<label className='label'>Email*</label><br />
+    <input placeholder='email' type='text' />
+</div>  
+
+<div className='contact-div'>
+<label className='label'>Write a message to me*</label><br />
+    <textarea placeholder='message' rows='10' cols='28'></textarea>
+</div>
+
+<div className='contact-div'>
+<input  type='submit' value='SUBMIT' style={{border:"none", padding:"10px 20px",borderRadius:"5px"}}/>
+</div>
+
+</form>
+
+        </Container>
+    </div>
+  )
+}
+
+export default ContactInfo
